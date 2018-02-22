c8b2302a0b92997cf4ffc9191c0578caf1b562a1
Lai Wei <laiwei@Lais-MacBook-Pro.local>
2018-02-22 00:46:10 -0800

    not sure why jwt gives error...comment out...

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -19,9 +19,14 @@
            Login
         </v-btn>
         <v-btn
-        v-if="$store.state.isUserLoggedIn"
+        v-if="!$store.state.isUserLoggedIn"
         flat dark @click="navigateTo({name: 'register'})">
            Sign up
+        </v-btn>
+        <v-btn
+        v-if="$store.state.isUserLoggedIn"
+        flat dark @click="logout">
+           Log Out
         </v-btn>
     </v-toolbar-items>
   </v-toolbar>
@@ -32,6 +37,13 @@ export default {
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'root'
+      })
     }
   }
 }
